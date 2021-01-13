fd6da392cd732c265ec643be982375c4c6bd48ca
Gabriel Lopes <gabrielperinotto@hotmail.com>
2021-01-13 09:13:05 -0300

    Removendo Versionamento

--- views-cache/footer.5d62c834414798429fd148609b6b01e6.rtpl.php
+++
@@ -1,9 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?>
-      <!--JavaScript at end of body for optimized loading-->
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-      <p style="position: fixed; bottom: 0; width: 100%;background-color: #333;color: white  list-style-type: none; margin: 0;padding: 0;
-overflow: hidden; color:white;"><i class="material-icons">explore</i> GitHub: <a href="https://github.com/Gabrielopes01" target="_blank">Gabrielopes01</a>
-<a class="grey-text text-lighten-4 right" href="#!" >@2021</a>
-</p>
-</body>
-</html>
--- views-cache/home.5e5951b1000324eb0a0d7a01c1cb719c.rtpl.php
+++
@@ -1,4 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><h1>Esta é a Home do Site</h1>
-<?php if( $nome != '' ){ ?>
-<p>Bem Vindo(a) <?php echo htmlspecialchars( $nome, ENT_COMPAT, 'UTF-8', FALSE ); ?></p>
-<?php } ?>
