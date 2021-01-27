6bf494cdeaed88ca5dc03406a617e42fd9230fbe
Gabriel Lopes <gabrielperinotto@hotmail.com>
2021-01-27 16:32:35 -0300

    Inicio da Paginação das noticias

--- home.php
+++ home.php
@@ -6,15 +6,33 @@ Use \Classes\News;
 //Home de Site
 $app->get("/", function(){
 
+    header("Location: /0");
+    exit;
+
+});
+
+$app->get("/:num", function($num){
+
     $page = new Page([
         "nome"=>isset($_SESSION['nome'])? $_SESSION['nome']:''
     ]);
 
     $noticias = News::getALLNews();
 
+    $resultado = [];
+
+    for ($i = 24 * $num; $i < 24 * ($num + 1); $i++) {
+        if($i == count($noticias) || $i > count($noticias)){
+            break;
+        } else {
+            array_push($resultado, $noticias[$i]);
+        }
+    }
+
+
     $page->setTpl('home', [
         "nome"=>isset($_SESSION['nome'])? $_SESSION['nome']:'',
-        "noticias"=>$noticias
+        "noticias"=>$resultado
     ]);
 
 });
