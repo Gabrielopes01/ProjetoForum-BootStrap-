932da9dff895d0db019c1149bc5bc272e52f2569
Gabriel Lopes <gabrielperinotto@hotmail.com>
2021-02-09 14:49:36 -0300

    Testes com Namespace

---
+++ model/php-classes/src/Teste.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Novo;
+
+class Teste {
+
+    public static function say(){
+
+        echo "<strong>Hello World</strong>";
+        echo"<br>Funcionou o Namespace";
+        exit;
+
+    }
+
+}
+
+
+?>
