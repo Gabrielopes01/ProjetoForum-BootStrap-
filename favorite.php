8ca0f1362915e3490ae0ef52fe55fca7c3d52045
Gabriel Lopes <gabrielperinotto@hotmail.com>
2021-02-17 14:48:50 -0300

    Adição de AJAX nos Favoritos

--- favorite.php
+++ favorite.php
@@ -2,56 +2,16 @@
 
 Use \Classes\Favorite;
 Use \Classes\Page;
+Use \Classes\User;
 require_once("function.php");
-
-/*
-$app->get("/:pag/favorite/:id", function($pag, $id){
-
-    $ver = Favorite::verifyFavorite($id);
-
-    if($ver == 1){
-        Favorite::removeFavorite($id);
-        header("Location: /$pag");
-        exit;
-    } else {
-        Favorite::addFavorites($id);
-        header("Location: /$pag");
-        exit;
-    }
-
-});
-
-$app->get("/:pag/favoriteOne/:id", function($pag, $id){
-
-    $ver = Favorite::verifyFavorite($id);
-
-    if($ver == 1){
-        Favorite::removeFavorite($id);
-        header("Location: /news/$id");
-        exit;
-    } else {
-        Favorite::addFavorites($id);
-        header("Location: /news/$id");
-        exit;
-    }
-
-});
-
-$app->get("/:pag/favoriteRemove/:id", function($pag, $id){
-
-    Favorite::removeFavorite($id, $pag);
-
-    header("Location: /$pag/myFavorites");
-    exit;
-
-});
-*/
 
 $app->get("/:pag/myFavorites", function($pag){
 
     $page = new Page([
         "nome"=>isset($_SESSION['nome'])? $_SESSION['nome']:''
     ]);
+
+    User::checkLogin();
 
     $favoritos = Favorite::getALLFavorites();
 
